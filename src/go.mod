30f546eaf5a65243f4a29e83adcf7dfc079790cb
sky0621 <sky14work@gmail.com>
2020-10-07 01:21:08 +0900

    temp

--- src/go.mod
+++ src/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	cloud.google.com/go v0.68.0 // indirect
+	cloud.google.com/go/firestore v1.3.0
 	cloud.google.com/go/pubsub v1.8.1
 	github.com/google/uuid v1.1.1
 	github.com/labstack/echo/v4 v4.1.17
--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"time"
 
+	"cloud.google.com/go/firestore"
 	"cloud.google.com/go/pubsub"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
@@ -21,37 +22,138 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/add-school", func(c echo.Context) error {
-		newID := createUUID()
-		sendTopic(c.Request().Context(), project, "add-school:"+newID)
-		return c.String(http.StatusOK, newID)
-	})
+	e.GET("/add-region", regionHandler(project, "add-region"))
+	e.GET("/edit-region/:rid", regionHandler(project, "edit-region"))
 
-	e.GET("/add-grade", func(c echo.Context) error {
-		newID := createUUID()
-		sendTopic(c.Request().Context(), project, "add-grade:"+newID)
-		return c.String(http.StatusOK, newID)
-	})
+	e.GET("/add-school/:rid", schoolHandler(project, "add-school"))
+	e.GET("/edit-school/:rid/:sid", schoolHandler(project, "edit-school"))
 
-	e.GET("/add-class", func(c echo.Context) error {
-		newID := createUUID()
-		sendTopic(c.Request().Context(), project, "add-class:"+newID)
-		return c.String(http.StatusOK, newID)
-	})
-
-	e.GET("/add-teacher", func(c echo.Context) error {
-		newID := createUUID()
-		sendTopic(c.Request().Context(), project, "add-teacher:"+newID)
-		return c.String(http.StatusOK, newID)
-	})
-
-	e.GET("/add-student", func(c echo.Context) error {
-		newID := createUUID()
-		sendTopic(c.Request().Context(), project, "add-student:"+newID)
-		return c.String(http.StatusOK, newID)
-	})
+	e.GET("/add-grade/:rid/:sid", handler(project, "add-grade"))
+	e.GET("/add-class/:rid/:sid", handler(project, "add-class"))
+	e.GET("/add-teacher/:rid/:sid", handler(project, "add-teacher"))
+	e.GET("/add-student/:rid/:sid", handler(project, "add-student"))
 
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func regionHandler(project, path string) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+		regionID := c.Param("rid")
+		if regionID == "" {
+			regionID = createUUID()
+		}
+		operationSequence := createOperationSequence()
+
+		client, err := firestore.NewClient(ctx, project)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer func() {
+			if client != nil {
+				if err := client.Close(); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}()
+
+		_, err = client.Collection("region").Doc(regionID).
+			Set(ctx, map[string]interface{}{
+				"operationSequence": operationSequence,
+				"order":             path + ":" + regionID,
+				"isDone":            false,
+			}, firestore.MergeAll)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sendTopic(ctx, project, path+":"+regionID, operationSequence)
+		return c.String(http.StatusOK, regionID)
+	}
+}
+
+func schoolHandler(project, path string) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+		regionID := c.Param("rid")
+		if regionID == "" {
+			log.Fatal("no regionID")
+		}
+		schoolID := c.Param("sid")
+		if schoolID == "" {
+			schoolID = createUUID()
+		}
+		operationSequence := createOperationSequence()
+
+		client, err := firestore.NewClient(ctx, project)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer func() {
+			if client != nil {
+				if err := client.Close(); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}()
+
+		_, err = client.Collection("region").Doc(regionID).
+			Collection("school").Doc(schoolID).
+			Set(ctx, map[string]interface{}{
+				"operationSequence": operationSequence,
+				"order":             path + ":" + regionID + ":" + schoolID,
+				"isDone":            false,
+			}, firestore.MergeAll)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sendTopic(ctx, project, path+":"+regionID+":"+schoolID, operationSequence)
+		return c.String(http.StatusOK, regionID+":"+schoolID)
+	}
+}
+
+func handler(project, path string) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+		regionID := c.Param("rid")
+		if regionID == "" {
+			log.Fatal("no regionID")
+		}
+		schoolID := c.Param("sid")
+		if schoolID == "" {
+			log.Fatal("no schoolID")
+		}
+		newID := createUUID()
+		operationSequence := createOperationSequence()
+
+		client, err := firestore.NewClient(ctx, project)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer func() {
+			if client != nil {
+				if err := client.Close(); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}()
+
+		_, err = client.Collection("region").Doc(regionID).
+			Collection("school").Doc(schoolID).
+			Collection("operation").Doc(newID).
+			Set(ctx, map[string]interface{}{
+				"operationSequence": operationSequence,
+				"order":             path + ":" + regionID + ":" + schoolID + ":" + newID,
+				"isDone":            false,
+			}, firestore.MergeAll)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sendTopic(ctx, project, path+":"+regionID+":"+schoolID+":"+newID, operationSequence)
+		return c.String(http.StatusOK, regionID+":"+schoolID+":"+newID)
+	}
 }
 
 func createUUID() string {
@@ -62,7 +164,7 @@ func createUUID() string {
 	return u.String()
 }
 
-func sendTopic(ctx context.Context, project, order string) {
+func sendTopic(ctx context.Context, project, order, serializeKey string) {
 	client, err := pubsub.NewClient(ctx, project)
 	if err != nil {
 		log.Fatal(err)
@@ -74,12 +176,16 @@ func sendTopic(ctx context.Context, project, order string) {
 	topic.EnableMessageOrdering = true
 
 	message := &pubsub.Message{
-		OrderingKey: fmt.Sprintf("%d", time.Now().UnixNano()),
-		Data:        []byte(order),
+		OrderingKey: serializeKey,
+		Data:        []byte(order + ":" + serializeKey),
 	}
 	r := topic.Publish(ctx, message)
 	if r == nil {
 		log.Fatal("failed to topic.Publish!")
 	}
 	log.Printf("%+v", r)
+}
+
+func createOperationSequence() string {
+	return fmt.Sprintf("%d", time.Now().UnixNano())
 }
